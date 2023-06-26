5632f4e1b76cf2d97d367cbe8988c55a8bb41c2d
sanketspatel <mailto:sanket.patel@techybirds.com>
2023-06-26 09:48:48 +0530

    Removed controller and changed user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,10 +3,10 @@ const {ensureAuthorized} = require('../middleWare/authCheck');
 const apiUserController = require('../controller/user.controller');
 module.exports = (function () {
 // User Controller Route using HOST URL + /app-config/user
-    app.post("/add", ensureAuthorized, apiUserController.addUser);//addUser
-    app.post("/list", ensureAuthorized, apiUserController.listUsers);//viewUser
-    app.post("/:id", ensureAuthorized, apiUserController.viewUserById);//viewUser by id
-    app.post("/update", ensureAuthorized, apiUserController.updateUser);//updateUser
-    app.post("/delete", ensureAuthorized, apiUserController.deleteUser);//deleteUser
+    app.post("/", ensureAuthorized, apiUserController.addUser);//addUser
+    app.get("/", ensureAuthorized, apiUserController.listUsers);//viewUser
+    app.get("/:id", ensureAuthorized, apiUserController.viewUserById);//viewUser by id
+    app.put("/", ensureAuthorized, apiUserController.updateUser);//updateUser
+    app.delete("/", ensureAuthorized, apiUserController.deleteUser);//deleteUser
     return app;
 })();
